2a7ca4d9b2bab0257656a6314f51391b5e80660b
JesGraef <rad22003@byui.edu>
2023-01-05 18:37:58 -0400

    fix javascript dates

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,12 @@
-let modified = document.getElementById('modified');
-modified.textContent = new Date(document.lastModified);
+let date = new Date(document.lastModified);
+let shortDate = date.toLocaleDateString('en-US');
+let hours = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+let fullDate = `${shortDate} ${hours}:${min}:${sec}`
+document.getElementById("modified").innerHTML = fullDate;
+
+
+let currentDate = new Date()
+let currentYear = currentDate.getFullYear()
+document.getElementById('year').innerHTML = currentYear;
