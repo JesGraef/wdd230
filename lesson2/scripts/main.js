bae6fbaa54dd49d9b75943bc48b1c33866711084
JesGraef <rad22003@byui.edu>
2023-01-12 19:25:53 -0400

    start lesson 2 homework

---
+++ lesson2/scripts/main.js
@@ -0,0 +1,12 @@
+let date = new Date(document.lastModified);
+let shortDate = date.toLocaleDateString('en-US');
+let hours = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+let fullDate = `${shortDate} ${hours}:${min}:${sec}`
+document.getElementById("modified").innerHTML = fullDate;
+
+
+let currentDate = new Date()
+let currentYear = currentDate.getFullYear()
+document.querySelector('#year').innerHTML = currentYear;
