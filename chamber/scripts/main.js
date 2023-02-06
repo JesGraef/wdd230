30e1b0cc331f1b1e8b12b142d1ccc9b869d1104a
JesGraef <rad22003@byui.edu>
2023-02-06 14:34:36 -0400

    fix contrast colors

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,17 +1,14 @@
-// select the elements to manipulate (output to)
-// const datefield = document.querySelector(".date");
+// Display date on top
 const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
-
 // derive the current date using a date object
 const now = new Date();
-
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(now);
 // long, medium, short options ... try them
-
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
+//Display date on the footer
 const date = new Date(document.lastModified);
 const shortDate = date.toLocaleDateString('en-US');
 const hours = date.getHours();
@@ -20,16 +17,19 @@ const sec = date.getSeconds();
 const fullDate = `${shortDate} ${hours}:${min}:${sec}`
 document.getElementById("modified").innerHTML = fullDate;
 
+//Display year
 const currentDate = new Date()
 const currentYear = currentDate.getFullYear()
 document.querySelector('#year').innerHTML = currentYear;
 
+//Announcement on Mondays and Tuesdays
 const announcement = document.querySelector('#announcement');
 const today = new Date();
 if(today.getDay() == 1 || today.getDay() == 2) {
 	announcement.style.display = 'block';
 }
 
+//Hamburger
 function toggleMenu() {
 	document.getElementById('primaryNav').classList.toggle('open');
 	document.getElementById('hamburgerBtn').classList.toggle('open');
---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,13 @@
+const t = 35;
+const s = 3.5;
+
+document.querySelector('#temperature').textContent = t;
+document.querySelector('#speed').textContent = s;
+
+let f = "N/A";
+if (t <= 50 && s > 3.0) {
+    f = 35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s  ** 0.16
+}
+
+document.querySelector('#chill').textContent = Math.round(f);    
+
