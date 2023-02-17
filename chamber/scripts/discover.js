526820d386cacf23f9f8c4774e2ab27f1b0cae75
JesGraef <rad22003@byui.edu>
2023-02-17 13:00:24 -0400

    :money_with_wings: finally finish week 7!!!!!!!

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -26,9 +26,17 @@ if ("IntersectionObserver" in window) {
   imagesToLoad.forEach((img) => {
       imgObserver.observe(img);
   });
-} //else {
-  //imagesToLoad.forEach((img) => {
-      //loadImages(img);
-  //});
-//}
+} 
+
+// Local Storage
+
+const lastVisit = localStorage.getItem("lastVisitDate");
+if (!lastVisit) {
+  document.querySelector('#visits').textContent = `This is your first visit!`;
+  localStorage.setItem("lastVisitDate", new Date());
+} else {
+  console.log('lastVisit', lastVisit);
+  const daysFromLastVisit = Math.round(((new Date()).getTime() - (new Date(lastVisit)).getTime())/84600000);
+  document.querySelector('#visits').textContent = `It's been ${daysFromLastVisit} days since your last visit.`;
+}
   
