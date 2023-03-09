2ffe261652680cdde14caae1eb7ba0b9ef4d35d6
JesGraef <rad22003@byui.edu>
2023-03-09 11:39:40 -0400

    :green_heart: finish week 10!!!!!!

--- chamber/scripts/practice.js
+++ chamber/scripts/practice.js
@@ -1,5 +1,5 @@
 async function apiFetch() {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Rothesay,CA&units=imperial&appid=d5ff9422b0221d7c0207bd4aa38a8886';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad,US&units=imperial&appid=d5ff9422b0221d7c0207bd4aa38a8886';
     try {
       const response = await fetch(url);
       if (response.ok) {
@@ -16,18 +16,13 @@ async function apiFetch() {
 
 function displayResults(data) {
     const t = data.main.temp;
-    const s = data.wind.speed;
+    // const s = data.wind.speed;
     
     document.querySelector('#temperature').textContent = `${Math.round(t)}\u00B0C`;
     document.querySelector('#tempdesc').textContent = data.weather[0].description.toUpperCase();
-    document.querySelector('#weatherPic').src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    document.querySelector('#speed').textContent = s;
-
-    let f = "N/A";
-    if (t <= 50 && s > 3.0) {
-        f = 35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s  ** 0.16;
-        document.querySelector('#chill').textContent = Math.round(f);
-    } 
+    document.querySelector('#humidity').textContent = data.main.humidity;
+    // document.querySelector('#weatherPic').src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    // document.querySelector('#speed').textContent = s;
 }
 
 apiFetch();
