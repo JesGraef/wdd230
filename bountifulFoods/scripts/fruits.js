974678cda8d12d33c2c28799c8346eafda1ef78c
JesGraef <rad22003@byui.edu>
2023-03-27 23:18:44 -0300

    :bug: few fixes

--- bountifulFoods/scripts/fruits.js
+++ bountifulFoods/scripts/fruits.js
@@ -23,5 +23,57 @@ const displayFruitOptions = data => {
         fruitSelect2.appendChild(option2);
         fruitSelect3.appendChild(option3);
     })
+}
+//Display Input
+const displayInput = data => {
+    const name = document.querySelector("#display_name");
+    const email = document.querySelector("#display_email");
+    const phone = document.querySelector("#display_phone");  // select the output container element
+    
+    data.forEach(info => {
+        let input1 = document.createElement('option');
+        let input2 = document.createElement('option');
+        let input3 = document.createElement('option');
+        
+        input1.setAttribute('value', info.name);
+        input1.setAttribute('label', info.name);
+        input2.setAttribute('value', info.name);
+        input2.setAttribute('label', info.name);
+        input3.setAttribute('value', info.name);
+        input3.setAttribute('label', info.name);
 
-} 
+        name.appendChild(input1);
+        phone.appendChild(input2);
+        email.appendChild(input3);
+    })
+}
+// Hidden element on Join Page
+// const today = new Date();
+// document.querySelector('#dateTime').value = today;
+
+
+function store(){
+    const submissionsDisplay = document.querySelector(".submissions");
+
+    let numsubmissions = Number(window.localStorage.getItem("submission"));
+    if (numsubmissions !== 0) {
+        submissionsDisplay.textContent = numsubmissions;
+    } else {
+        submissionsDisplay.textContent = "This is your first order!"
+    }
+
+    numsubmissions++;
+    localStorage.setItem("submission", numsubmissions);
+}
+
+//Display Output
+const register = function (event) {
+    // Receive user input
+    let r_name = document.querySelector("#name").value;
+    let r_email = document.querySelector("#email").value;
+    // Output on page
+    display_name.innerHTML = r_name;
+    display_email.innerHTML = r_email;
+    event.preventDefault();
+    };
+
--- bountifulFoods/scripts/main.js
+++ bountifulFoods/scripts/main.js
@@ -13,14 +13,23 @@ hambutton.addEventListener('click', () => {
 // Site Last Updated
 const date = new Date(document.lastModified);
 const shortDate = date.toLocaleDateString('en-US');
-// const hours = date.getHours();
-// const min = date.getMinutes();
-// const sec = date.getSeconds();
-// const fullDate = `${shortDate} ${hours}:${min}:${sec}`
 document.getElementById("modified").innerHTML = shortDate;
 
 
 // Hidden element on Join Page
+// const today = new Date();
 // document.querySelector('#dateTime').value = today;
+
+// Drink Submissions
+const submissionsDisplay = document.querySelector(".submissions");
+let numsubmissions = Number(window.localStorage.getItem("submission"));
+if (numsubmissions !== 0) {
+    submissionsDisplay.textContent = numsubmissions;
+} else {
+    submissionsDisplay.textContent = "0"
+}
+
+numsubmissions++;
+localStorage.setItem("submission", numsubmissions);
 
 
